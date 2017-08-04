3647df8b7accde4f12c6100272b1c21c67a3207f
json-Li <jsonfront@163.com>
2017-08-04 18:15:06 +0800

    tt

--- test/js/trex.js
+++ test/js/trex.js
@@ -1,5 +1,5 @@
 
-
+//示范面向对象 this 作用域 闭包 单例模式很好的示范
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
